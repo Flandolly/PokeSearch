c34d58ceee36cf9eb7b8d7c39e9b00bb73864fc5
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 17:37:50 -0400

    Added header, Made home buttons, added bootstrap

--- pokesearch-app/src/App.js
+++ pokesearch-app/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Home from "./Components/Home";
 import {Route} from "react-router-dom";
+import Header from "./Components/Header";
 
 function App() {
-  return (
-    <div>
-      <Route path={"/"} component={Home}/>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <Route path={"/"} component={Header}/>
+            </header>
+
+            <main>
+                <Route path={"/"} exact component={Home}/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- pokesearch-app/src/Components/Home.js
+++ pokesearch-app/src/Components/Home.js
@@ -3,7 +3,17 @@ import React from "react";
 function Home() {
 
     return (
-        <h1>PokeSearch</h1>
+        <div className={"home-container container-fluid d-flex justify-content-center align-items-center flex-column"}>
+            <h1 className={"home-title"}>PokéSearch</h1>
+            <div className={"home-button-group container"}>
+                <div className={"row"}>
+                    <div className={"col text-center pokemon"}>Pokémon</div>
+                    <div className={"col text-center type"}>Type</div>
+                    <div className={"col text-center move"}>Move</div>
+                    <div className={"col text-center ability"}>Ability</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
