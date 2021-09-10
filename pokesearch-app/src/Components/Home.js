55767fed61b424fc4251b1de3790eef1a6b8a3af
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 18:53:08 -0400

    Added search form, onClick animations for request types, styling and animations

--- pokesearch-app/src/Components/Home.js
+++ pokesearch-app/src/Components/Home.js
@@ -2,17 +2,31 @@ import React from "react";
 
 function Home() {
 
+    function setButtonToActive(event) {
+        const buttons = document.querySelectorAll(".pokemon, .type, .move, .ability")
+
+        buttons.forEach((button) => {
+            button.classList.remove("active")
+        })
+
+        event.currentTarget.classList.add("active")
+    }
+
     return (
         <div className={"home-container container-fluid d-flex justify-content-center align-items-center flex-column"}>
             <h1 className={"home-title"}>PokéSearch</h1>
             <div className={"home-button-group container"}>
                 <div className={"row"}>
-                    <div className={"col text-center pokemon"}>Pokémon</div>
-                    <div className={"col text-center type"}>Type</div>
-                    <div className={"col text-center move"}>Move</div>
-                    <div className={"col text-center ability"}>Ability</div>
+                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center pokemon active"}>Pokémon</div>
+                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center type"}>Type</div>
+                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center move"}>Move</div>
+                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center ability"}>Ability</div>
                 </div>
             </div>
+            <form className={"search-form input-group mb-3 justify-content-center"}>
+                <input type={"text"} className={"form-control-lg"} placeholder={"Enter Name or ID"}/>
+                <button className={"btn btn-outline-dark"} type={"submit"} id={"submitBtn"}>Search</button>
+            </form>
         </div>
     )
 }
