7c2634b5a1948bb2f225dfb16d033d02eb649c32
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 19:27:39 -0400

    Added states and handlers for submit and input changing

--- pokesearch-app/src/Components/Home.js
+++ pokesearch-app/src/Components/Home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Home() {
+
+    const [inputString, setInputString] = useState("")
+    const [searchTerm, setSearchTerm] = useState([])
 
     function setButtonToActive(event) {
         const buttons = document.querySelectorAll(".pokemon, .type, .move, .ability")
@@ -12,19 +15,32 @@ function Home() {
         event.currentTarget.classList.add("active")
     }
 
+    function handleChange(input) {
+        return setInputString(input.toLowerCase())
+    }
+
+    function handleSubmit(event) {
+        const searchType = document.querySelector(".active").innerText.toLowerCase()
+
+        event.preventDefault()
+        document.getElementById("search-form").reset()
+        setSearchTerm([inputString, searchType])
+        console.log(searchTerm)
+    }
+
     return (
         <div className={"home-container container-fluid d-flex justify-content-center align-items-center flex-column"}>
             <h1 className={"home-title"}>PokéSearch</h1>
             <div className={"home-button-group container"}>
                 <div className={"row"}>
-                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center pokemon active"}>Pokémon</div>
+                    <div onClick={(e) => setButtonToActive(e)} className={"col text-center pokemon active"}>Pokemon</div>
                     <div onClick={(e) => setButtonToActive(e)} className={"col text-center type"}>Type</div>
                     <div onClick={(e) => setButtonToActive(e)} className={"col text-center move"}>Move</div>
                     <div onClick={(e) => setButtonToActive(e)} className={"col text-center ability"}>Ability</div>
                 </div>
             </div>
-            <form className={"search-form input-group mb-3 justify-content-center"}>
-                <input type={"text"} className={"form-control-lg"} placeholder={"Enter Name or ID"}/>
+            <form id={"search-form"} onSubmit={(e) => {handleSubmit(e)}} className={"search-form input-group mb-3 justify-content-center"}>
+                <input type={"text"} className={"form-control-lg"} placeholder={"Enter Name or ID"} value={inputString} onChange={(e) => handleChange(e.target.value)}/>
                 <button className={"btn btn-outline-dark"} type={"submit"} id={"submitBtn"}>Search</button>
             </form>
         </div>
