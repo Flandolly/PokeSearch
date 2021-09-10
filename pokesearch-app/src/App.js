5b1b1c7a215af4f466b8987f6526181cd5e36eab
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 16:40:45 -0400

    Added necessary fonts

--- pokesearch-app/src/App.js
+++ pokesearch-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from "./Components/Home";
+import {Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-
+      <Route path={"/"} component={Home}/>
     </div>
   );
 }
---
+++ pokesearch-app/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <h1>PokeSearch</h1>
+    )
+}
+
+export default Home
