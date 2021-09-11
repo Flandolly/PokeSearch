4a1b68a24a009cd6eabed25b8bb2bbf17db51968
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 21:14:53 -0400

    Made proper API fetch request through axios

--- pokesearch-app/src/Components/Pokemon.js
+++ pokesearch-app/src/Components/Pokemon.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-function Pokemon() {
+function Pokemon(props) {
+
+    const axios = require("axios").default
+    const [pokemon, setPokemon] = useState({})
+
+
+    useEffect(() => {
+        function getPokemon() {
+            const searchParams = props.match.url.split("/")
+
+            axios.get(`https://pokeapi.co/api/v2/${searchParams[1]}/${searchParams[2]}/`)
+                .then(function (response) {
+                    console.log(response.data)
+                    setPokemon(response.data)
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        }
+        getPokemon()
+    },[axios, props.match.url])
+
     return (
-        <h1>Gay</h1>
+        <div className={"container"}>
+            <section className={"main-body"}>
+                <div className={"error"}>
+                    {/*{!didErrorOccur ? <h1>No results found. Try again.</h1> : null}*/}
+                </div>
+
+            </section>
+            <section className={"lower-body"}>
+
+            </section>
+        </div>
     )
 }
 
