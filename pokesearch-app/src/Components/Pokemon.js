d1510f17fe9c970ac46dd3b5b4b30b5d0d845506
Christina Whethers <flandresamarawr@gmail.com>
2021-09-10 23:13:01 -0400

    Rendered Pokemon name, id, image and stats chart, added functionality to make sure the page doesn't try to render before fetching

--- pokesearch-app/src/Components/Pokemon.js
+++ pokesearch-app/src/Components/Pokemon.js
@@ -22,19 +22,63 @@ function Pokemon(props) {
         getPokemon()
     },[axios, props.match.url])
 
-    return (
-        <div className={"container"}>
-            <section className={"main-body"}>
-                <div className={"error"}>
-                    {/*{!didErrorOccur ? <h1>No results found. Try again.</h1> : null}*/}
+    /*
+    Check to prevent data for images being loaded before the API data is finished being fetched.
+    Sourced from: https://stackoverflow.com/questions/63281536/react-hooks-how-to-wait-for-the-data-to-be-fetched-before-rendering
+     */
+
+    if (pokemon.sprites !== undefined) {
+        return (
+            <div className={"container-fluid"}>
+                <section className={"main-body d-flex align-items-center flex-column"}>
+                    <div className={"error"}>
+                        {/*{!didErrorOccur ? <h1>No results found. Try again.</h1> : null}*/}
+                    </div>
+
+                    <div className={"pokemon-title text-capitalize"}>
+                        <h3>{pokemon.name}</h3>
+                    </div>
+                    <div className={"pokemon-id"}>
+                        <h4>No.{pokemon.id}</h4>
+                    </div>
+                </section>
+                <div className={"pokemon-imgs d-flex justify-content-evenly"}>
+                    {/*<img src={pokemon.sprites.front_default} alt={pokemon.name}/>*/}
+                    {/*<img src={pokemon.sprites.back_default} alt={pokemon.name}/>*/}
+                    <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name}/>
+                    <div className={"stats-chart align-self-center"}>
+                        <div className={"container"}>
+                            <div className={"row row-cols-2"}>
+                                <div className="stat col text-center">{pokemon.stats[0].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[0].base_stat}</div>
+                                <div className="stat col text-center">{pokemon.stats[1].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[1].base_stat}</div>
+                                <div className="stat col text-center">{pokemon.stats[2].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[2].base_stat}</div>
+                                <div className="stat col text-center">{pokemon.stats[3].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[3].base_stat}</div>
+                                <div className="stat col text-center">{pokemon.stats[4].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[4].base_stat}</div>
+                                <div className="stat col text-center">{pokemon.stats[5].stat.name.toUpperCase()}</div>
+                                <div className="stat col text-center">{pokemon.stats[5].base_stat}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <section className={"lower-body"}>
 
-            </section>
-            <section className={"lower-body"}>
-
-            </section>
-        </div>
-    )
+                </section>
+            </div>
+        )
+    } else {
+        return (
+            <div className={"container-fluid"}>
+                <div className={"main-body d-flex justify-content-center align-items-center"}>
+                    <h1>Loading...</h1>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Pokemon
