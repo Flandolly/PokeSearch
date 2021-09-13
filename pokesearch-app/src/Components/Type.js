3ba1d51b417022969858705195fae3145092fc83
Christina Whethers <flandresamarawr@gmail.com>
2021-09-12 22:58:54 -0400

    Added section for type matchups

--- pokesearch-app/src/Components/Type.js
+++ pokesearch-app/src/Components/Type.js
@@ -25,10 +25,10 @@ function Type(props) {
 
     const [type, setType] = useState({})
     const [image, setImage] = useState("")
-    const imageSrcs = [iconDark, iconIce, iconGrass, iconNormal, iconFairy, iconGround, iconDragon, iconFighting, iconRock,
-        iconPsychic, iconFire, iconFire, iconWater, iconElectric, iconPoison, iconBug, iconFlying, iconSteel, iconGhost]
 
     useEffect(() => {
+        const imageSrcs = [iconDark, iconIce, iconGrass, iconNormal, iconFairy, iconGround, iconDragon, iconFighting, iconRock,
+            iconPsychic, iconFire, iconFire, iconWater, iconElectric, iconPoison, iconBug, iconFlying, iconSteel, iconGhost]
         const searchParams = props.match.url.split("/")
         axios.get(`https://pokeapi.co/api/v2/${searchParams[1]}/${searchParams[2]}/`)
             .then(function (response) {
@@ -41,32 +41,82 @@ function Type(props) {
     }, [props.match.url])
 
 
-    return (
-        <div className={"container-fluid"}>
-            <section className={"main-body d-flex align-items-center flex-column"}>
-                <div className={"type-title text-capitalize"}>
-                    <h3>{type.name} Type</h3>
-                </div>
-            </section>
-            <div className={"d-flex justify-content-evenly"}>
-                <div className={"type-imgs"}>
-                    <img className={"type-img"} src={image} alt={type.name}/>
-                </div>
-                <div className={"pokemon-group d-flex flex-column"}>
-                    <h4 className={"pokemon-list-title fs-6 text-center"}>Pokemon With This Type</h4>
-                    <div className={"pokemon-list d-flex flex-row flex-wrap"}>
-                        {type.pokemon.map((poke, idx) => {
-                            return (
-                                <div className={"pokemon"}>
-                                    <PokemonList poke={poke}/>
-                                </div>
-                            )
-                        })}
+    if (type.pokemon !== undefined) {
+        return (
+            <div className={"container-fluid"}>
+                <section className={"main-body d-flex align-items-center flex-column"}>
+                    <div className={"type-title text-capitalize"}>
+                        <h3>{type.name} Type</h3>
+                    </div>
+                </section>
+                <div className={"d-flex justify-content-evenly"}>
+                    <div className={"type-imgs"}>
+                        <img className={"type-img"} src={image} alt={type.name}/>
+                    </div>
+                    <div className={"pokemon-group d-flex flex-column"}>
+                        <h4 className={"pokemon-list-title fs-6 text-center"}>Pokemon With This Type</h4>
+                        <div className={"pokemon-list d-flex flex-row flex-wrap"}>
+                            {type.pokemon.map((poke, idx) => {
+                                return (
+                                    <div className={"pokemon"}>
+                                        <PokemonList poke={poke}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
+                <section className={"lower-body d-flex flex-row justify-content-evenly"}>
+                    <div className={"matchups d-flex"}>
+                        <div className={"strengths"}>
+                            <h4>Strong To:</h4>
+                            {!type.damage_relations.double_damage_to.length ?
+                                <h5 className={"text-muted fst-italic text-center"}>None</h5> : type.damage_relations.double_damage_to.map((item, idx) => {
+                                    return (
+                                        <div className={"type"}>
+                                            <h5 className={"text-capitalize text-center"}>{item.name}</h5>
+                                        </div>
+                                    )
+                                })}
+                        </div>
+                        <div className={"weaknesses"}>
+                            <h4>Weak To:</h4>
+                            {!type.damage_relations.double_damage_from.length ?
+                                <h5 className={"text-muted fst-italic text-center"}>Empty.</h5> : type.damage_relations.double_damage_from.map((item, idx) => {
+                                    return (
+                                        <div className={"type"}>
+                                            <h5 className={"text-capitalize text-center"}>{item.name}</h5>
+                                        </div>
+                                    )
+                                })}
+                        </div>
+                        <div className={"immunities"}>
+                            <h4>Immune To:</h4>
+                            {!type.damage_relations.no_damage_from.length ?
+                                <h5 className={"text-muted fst-italic text-center"}>Empty</h5> : type.damage_relations.no_damage_from.map((item, idx) => {
+                                    return (
+                                        <div className={"type"}>
+                                            <h5 className={"text-capitalize text-center"}>{item.name}</h5>
+                                        </div>
+                                    )
+                                })}
+                        </div>
+                    </div>
+                    <div className={"move-list"}>
+
+                    </div>
+                </section>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className={"container-fluid"}>
+                <div className={"main-body d-flex justify-content-center align-items-center"}>
+                    <h1>Loading...</h1>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Type
