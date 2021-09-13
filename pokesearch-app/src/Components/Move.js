d170e94c8457b40a171f4e06cc0de85296f7c314
Christina Whethers <flandresamarawr@gmail.com>
2021-09-13 11:32:15 -0400

    Added the rest of structure for move page, including description and stat chart

--- pokesearch-app/src/Components/Move.js
+++ pokesearch-app/src/Components/Move.js
@@ -27,7 +27,20 @@ function Move(props) {
                         <h4>No. {move.id}</h4>
                     </div>
                 </section>
-                <section className={"middle-body"}>
+                <section className={"middle-body d-flex justify-content-evenly align-items-center"}>
+                    <div className={"stats-chart move-stats"}>
+                        <div className={"container"}>
+                            <h4 className={"text-center fs-6"}>Stats</h4>
+                            <div className={"row row-cols-2"}>
+                                <div className={"stat col text-center"}>Category:</div>
+                                <div className={"stat col text-center text-capitalize"}>{move.damage_class.name}</div>
+                                <div className={"stat col text-center"}>Power:</div>
+                                <div className={"stat col text-center"}>{move.power}</div>
+                                <div className={"stat col text-center"}>PP:</div>
+                                <div className={"stat col text-center"}>{move.pp}</div>
+                            </div>
+                        </div>
+                    </div>
                     <div className={"pokemon-group d-flex flex-column align-items-center"}>
                         <h4 className={"pokemon-list-title fs-6 text-center"}>Pokemon That Can Learn Move</h4>
                         <div className={"pokemon-list d-flex flex-row flex-wrap"}>
@@ -39,6 +52,14 @@ function Move(props) {
                                 )
                             })}
                         </div>
+                    </div>
+                </section>
+                <section className={"lower-body d-flex flex-column align-items-center"}>
+                    <div className={"desc-title"}>
+                        <h4 className={"fs-5 text-center"}>Description</h4>
+                    </div>
+                    <div className={"description"}>
+                        <h5>{move.effect_entries.find(entry => entry.language.name === "en").effect.replaceAll("$effect_chance", move.effect_chance)}</h5>
                     </div>
                 </section>
             </div>
